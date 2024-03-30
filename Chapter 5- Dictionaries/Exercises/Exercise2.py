b827882c4de549432ec0cf4f99a733bbaae7162f
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:26:59 +0400

    Create Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,20 @@
+#making glossary
+
+glossary = {
+    "variable": "A container that holds a value or information.",
+    "function": "A way to organize and reuse code that is used to perform a single, related action.",
+    "loop": "A way to repeat a block of codemultiple times.",
+    "list": "A data structure that is a collection of elements, each identified by at least one index or key.",
+    "float": "A data type with whole numbers and decimal numbers"
+}
+ #print glossary
+print  ("variable -", glossary['variable'], "\n")
+print  ("function -", glossary['function'], "\n")
+print  ("loop -", glossary['lopp'], "\n")
+print  ("List -", glossary['List'], "\n")
+print  ("float -", glossary['float'], "\n")
+
+
+
+
+
