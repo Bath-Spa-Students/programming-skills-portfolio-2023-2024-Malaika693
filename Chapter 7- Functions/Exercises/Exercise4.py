55288abf9a3ccc73b9a0e8918617fad1e251e9cf
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:34:27 +0400

    Create Exercise4.py

---
+++ Chapter 7- Functions/Exercises/Exercise4.py
@@ -0,0 +1,9 @@
+#function with size & message
+def make_shirt(size="Large",message="I love Python"):
+    print (f"The size of the shirt is {size} with the text design of {message} written on it.")
+#large shirt with default message 
+make_shirt()
+#medium shirt with default message
+make_shirt("Medium")
+#small shirt with personalised message
+make_shirt("Small","code for fun")
