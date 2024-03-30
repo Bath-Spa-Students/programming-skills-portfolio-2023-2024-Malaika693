74216fc3e2b365733a0b072ce4a40ee891cc353b
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:18:53 +0400

    Create Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,7 @@
+#invitation guest list
+guest_list = ["Rinoa", "Sofia", "marwa"]
+#print each name and message
+for guest in guest_list:
+    print(f'''Dear {guest}, \nyou are cordially invited to dinner at my house,
+i would be honored to have you join me for an delightful conversation and great food,
+ \n\nBest regards,\n[Malaika noor]''')
