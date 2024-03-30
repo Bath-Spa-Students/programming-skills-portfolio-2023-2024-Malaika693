3e9c667ade8490212f22d0e3722bdebf6e317b80
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:17:33 +0400

    Create Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,7 @@
+names = ["marwa", "alia", "sofia", "rinoa"]
+
+#personalized messages
+print (f"Hi {names [0]}! I hope you're feeling happy ")
+print (f"Hi {names [1]}! i wish you happy birthday ")
+print (f"Hi {names [2]}! I hope you've a good day ")
+print (f"Hi {names [3]}! all the best ")
