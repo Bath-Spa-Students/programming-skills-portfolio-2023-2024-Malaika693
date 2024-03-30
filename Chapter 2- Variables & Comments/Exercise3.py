796256ef908db4bc0414c10c679831993ac828d1
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:12:31 +0400

    Create Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercise3.py
@@ -0,0 +1,10 @@
+#representing & printing a persons name
+name = "\t Malaika noor \n"
+print(name)
+
+#Name with rlstrip, rstrip, strip
+print( name.lstrip()) 
+print( name.rstrip())
+print(name.strip())
+
+
