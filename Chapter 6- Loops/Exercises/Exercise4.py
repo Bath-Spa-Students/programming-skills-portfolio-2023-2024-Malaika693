b3dc9318136c453c14c69ad03f4599876a539ad7
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:31:46 +0400

    Create Exercise4.py

---
+++ Chapter 6- Loops/Exercises/Exercise4.py
@@ -0,0 +1,24 @@
+#  sandwich_orders list
+sandwich_orders = ['tuna', 'turkey', 'ham', 'vegetable']
+
+# finished_sandwiches list
+finished_sandwiches = []
+
+# Loop through the sandwich_orders list
+for sandwich in sandwich_orders:
+    # Make the sandwich
+    print("I made your", sandwich, "sandwich.")
+    # Move it to the finished_sandwiches list
+    finished_sandwiches.append(sandwich)
+
+# Print the finished sandwiches
+print("Here are the sandwiches that were made:")
+for sandwich in finished_sandwiches:
+    print(sandwich)
+
+
+
+
+
+
+    
