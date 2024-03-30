65ddc74b06d23ea9003307830c845676642b735d
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:34:03 +0400

    Create Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,9 @@
+def make_shirt(size, message):
+    print (f"The size of the shirt is {size} and the message prinyted on it")
+
+# Call the function once using positional arguments
+make_shirt("small", "I love myself!")
+
+# Call the function a second time using keyword arguments
+make_shirt(size="medium", message="Keep calm and code on!")
+
