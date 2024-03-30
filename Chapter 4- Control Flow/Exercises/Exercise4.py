9213b95fb343cd865d8fc031f018e3db7b3429c8
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:25:20 +0400

    Create Exercise4.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise4.py
@@ -0,0 +1,18 @@
+var_age = int(input ("25"))
+
+#if,elif,else staments
+
+if var_age < 2:
+    print("The person is a baby.")
+elif var_age < 4:
+    print("The person is a toddler.")
+elif var_age < 13:
+    print("The person is a kid.")
+elif var_age < 20:
+    print("The person is a teenager.")
+elif var_age < 65:
+    print("The person is an adult.")
+else:
+    print("The person is an elder.")
+
+
