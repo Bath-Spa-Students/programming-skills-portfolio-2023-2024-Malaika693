23679d773e5680006fa66dfaa9b7cb8f7f210e7b
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:33:38 +0400

    Create Exercise2.py

---
+++ Chapter 7- Functions/Exercises/Exercise2.py
@@ -0,0 +1,7 @@
+#book title & message
+def favorite_book(book_title):
+    msg = f"My favourite book to read is {book_title}"
+    print (msg)
+
+#calling function and adding book title
+favorite_book("After we fell")
