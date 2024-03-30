a7842ed28018602f21f8a76a65e0c18be6fba88e
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:26:22 +0400

    Create Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,12 @@
+#dictionaries
+person = {
+    "first_name": "Sofia",
+    "last_name": "Wasim",
+    "age": 25,
+    "city": "Dubai"
+}
+#print dictionaries
+print("First Name:", person["first_name"])
+print("Last Name:", person["last_name"])
+print("Age:", person["age"])
+print("City:", person["city"])
