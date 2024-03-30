1e8d45ba7b374b0953b75a208ed2d7517d544e62
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:33:07 +0400

    Create Exercise1.py

---
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -0,0 +1,6 @@
+#function to display messages
+def display_message():
+    print("In this chapter, we are learning about functions, how to write functions .")
+
+# Call the function
+display_message()
