2af472c1770102389377fb9617e1b1f281de4a23
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:18:26 +0400

    Create Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,8 @@
+#list of favorite cars
+favorite_transportation = ["Chevrolet camaro", "BMW M5", "Mercedes G63", "Mazda MX-5"]
+ #Printing each items in the list with a message
+for vehicle in favorite_transportation:
+    print (f"My favourite car is the {favorite_transportation[0]}") 
+print (f"I hope i can get one in the future.{favorite_transportation[1]}")
+print (f"I love {favorite_transportation[2]}")
+print (f"My best friend has {favorite_transportation[3]}")
