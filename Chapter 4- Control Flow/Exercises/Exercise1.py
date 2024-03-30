eb1b0ba8dad7d566ec3e90f2579961976b3f0519
Malaika693 <158833758+Malaika693@users.noreply.github.com>
2024-03-30 10:23:39 +0400

    Create Exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,11 @@
+#If test passed
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
+
+ #If test fail
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
